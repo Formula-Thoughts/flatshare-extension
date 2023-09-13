230de11a177eb7a5a57871dd001037c3dd226126
roboc <aidanwilliamgannon@gmail.com>
2023-09-13 12:28:59 +0100

    create group - add logic to create group - add get and create groups endpoints - add db to template - add create and get groups endpoint

--- server/src/app.py
+++ server/src/app.py
@@ -1,9 +1,68 @@
+import json
+import uuid
+
+import boto3
+from botocore.client import BaseClient
+
+
 def lambda_handler(event, context):
-    return {
-            "statusCode": 200,
-            "body": "{\"field\": 2}",
-            "headers": {"Content-Type": "application/json",
-                        'Access-Control-Allow-Origin': "*",
-                        "Access-Control-Allow-Headers": "*",
-                        "Access-Control-Allow-Methods": "*"}
+    route = event['routeKey']
+
+    s3_client = boto3.client('s3')
+
+    default_headers = {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Methods': '*'
     }
+
+    routes = {
+        'POST /groups': lambda data_client, x: create_group(data_client, x),
+        'GET /groups': lambda data_client, x: get_all_groups(data_client, x)
+    }
+
+    try:
+        (response, status) = routes[route](s3_client, event)
+        return {
+            'statusCode': status,
+            'body': json.dumps(response),
+            'headers': default_headers
+        }
+    except Exception:
+        return {
+            'statusCode': 500,
+            'body': '{\'message\': \'internal server error :(\'}',
+            'headers': default_headers
+        }
+
+
+def create_group(data_client, event) -> (dict, int):
+    id = uuid.uuid4()
+    body = json.loads(event['body'])
+    group = {
+        'id': id,
+        'name': body['name'],
+        'code': body['code']
+    }
+    data_client.put_object(Bucket='flatini-blob-db',
+                           Key=f'groups/{id}',
+                           Body=json.dumps(group),
+                           ContentType='application/json',)
+
+    return group
+
+
+def get_all_groups(data_client, event) -> (dict, int):
+    return [
+        {
+            'id': uuid.uuid4(),
+            'name': 'test1',
+            'code': 12345
+        },
+        {
+            'id': uuid.uuid4(),
+            'name': 'test2',
+            'code': 12346
+        }
+    ]
