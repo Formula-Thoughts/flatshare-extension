4cfc8e5c047cba4f06a0dd083673cdc94e0a0ad1
roboc <aidanwilliamgannon@gmail.com>
2023-09-14 16:09:43 +0100

    add methods for bucket

--- server/src/data/__init__.py
+++ server/src/data/__init__.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import Protocol
 
+import boto3
+
 
 class S3ObjectContent(Protocol):
 
@@ -25,25 +27,33 @@ class S3ObjectRefWrapper(Protocol):
 
 class S3ClientWrapper:
 
+    def __init__(self):
+        self.__s3_client = boto3.client("s3")
+
     def put_object(self,
                    bucket: str,
                    key: str,
                    body: str,
                    content_type: str) -> None:
-        ...
+        self.__s3_client.put_object(Bucket=bucket,
+                                    Key=key,
+                                    Body=body,
+                                    ContentType=content_type)
 
     def list_objects_v2(self,
                         bucket: str,
                         prefix: str) -> list[S3ObjectRefWrapper]:
-        ...
+        return self.__s3_client.list_objects_v2(Bucket=bucket,
+                                                Prefix=prefix)
 
     def get_object(self,
                    bucket: str,
-                   key: str) -> None:
-        ...
+                   key: str) -> S3Object:
+        return self.__s3_client.list_objects_v2(Bucket=bucket,
+                                                Key=key)
 
     def delete_object(self,
                       bucket: str,
                       key: str) -> None:
-        ...
-
+        self.__s3_client.delete_object(Bucket=bucket,
+                                       Key=key)
