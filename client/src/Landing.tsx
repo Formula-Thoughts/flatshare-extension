add5e37461c0945ca454492313937e0cc5dfb412
Dom Farr <dominicfarr@gmail.com>
2023-09-19 12:05:22 -0300

    Add join existing group ======================= Add input for group id Add button to join group Add ux to disable button/link when group id is not provided

--- client/src/Landing.tsx
+++ client/src/Landing.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { SetStateAction, useState } from "react";
 import styled from "styled-components";
 import Button from "./Button";
 import { useNavigate } from "react-router-dom";
@@ -7,13 +7,12 @@ const Wrapper = styled.div`
   position: fixed;
   height: 100%;
   width: 100%;
-  top: 0;
-  left: 0;
   display: flex;
   flex-direction: column;
+  gap: 10px;
 `;
 
-const Block = styled.div`
+const Block = styled.div<{ $bgColor?: string; $gap?: string }>`
   flex: 1;
   display: flex;
   flex-direction: column;
@@ -24,6 +23,21 @@ const Block = styled.div`
   p {
     max-width: 245px;
   }
+  background-color: ${(props) => props.$bgColor || "transparent"};
+  gap: ${(props) => props.$gap || "10px"};
+`;
+
+const Input = styled.input<{ $topMargin?: string }>`
+  background-color: transparent;
+  border: 2px solid #ccc;
+  padding: 20px;
+  width: 200px;
+  border-radius: 10px;
+  margin-top: ${(props) => props.$topMargin || "0"};
+  &::placeholder {
+    text-align: center;
+  }
+  color: #ccc;
 `;
 
 enum ScreenTypes {
@@ -40,21 +54,33 @@ export const Landing = () => {
   const navigate = useNavigate();
   const handleOnClick = () => navigate("/CreateGroup");
 
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setInputValue(e.target.value);
+  };
+
   return (
     <Wrapper>
-      <Block style={{ marginBottom: -100 }}>
+      <Block $gap="20px">
         <p>
           Create a group with your flatmates and start sharing links to find
           your next flat.
         </p>
+        <Button onClick={handleOnClick}>Create a new group</Button>
       </Block>
-      <Block>
-        <Button onClick={handleOnClick} style={{ marginTop: 30 }}>
-          Create a new group
-        </Button>
-      </Block>
-      <Block style={{ backgroundColor: "#322848" }}>
+      <Block $bgColor="#322848">
         <p>Join an existing group one of your flatmates has already created</p>
+        <Input
+          $topMargin="10px"
+          type="text"
+          placeholder="Enter the group ID"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <Button $disabled={inputValue === ""}>Join existing group</Button>
       </Block>
     </Wrapper>
   );
