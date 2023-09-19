7ee31e3cfe5c3d5c52d4f2793ce4018433f9dbfe
Dom Farr <dominicfarr@gmail.com>
2023-09-18 22:06:29 -0300

    Clear(er) structure in App ========================== Remove unnecessary code from App.tsx Improve semantic structure of App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,21 @@
-import React from "react";
 import Logo from "./assets/flatini-logo.png";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Landing } from "./Landing";
-import styled from "styled-components";
 import CreateGroup from "./CreateGroup";
-
-const Header = styled.div``;
 
 function App() {
   return (
     <>
-      <Header>
-        <img style={{ width: 110 }} src={Logo} />
-      </Header>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/CreateGroup" element={<CreateGroup />} />
-      </Routes>
+      <header>
+        <img style={{ width: 110 }} src={Logo} alt="DOM WAS HERE" />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/CreateGroup" element={<CreateGroup />} />
+        </Routes>
+      </main>
     </>
   );
 }
