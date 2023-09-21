8d39dedf26de73f05cbb35746d38c68fd8b5b703
Xavier Mod <hi.xavier.mod@gmail.com>
2023-09-21 08:51:33 +0100

    Add FlatView implementation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,35 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 import { Landing } from "./Landing";
 import CreateGroup from "./CreateGroup";
 import { Flats } from "./Flats";
+import FlatView from "./FlatView";
 
 function App() {
   const navigate = useNavigate();
   function gotoLanding(): void {
     navigate("/");
   }
+
+  const defaultNavigation = (url: string | undefined) => {
+    if (url?.includes("https://www.rightmove.co.uk/properties/")) {
+      navigate("/FlatView");
+    } else {
+      navigate("/");
+    }
+  };
+
+  chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
+    console.log("updating tab2", tab);
+    defaultNavigation(tab.url);
+  });
+
+  // Reads changes when active tab changes
+  chrome.tabs.onActivated.addListener(function (activeInfo) {
+    // Gets the URL of the active tab
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+      let url = tabs[0].url;
+      defaultNavigation(url);
+    });
+  });
 
   return (
     <>
@@ -20,6 +43,7 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/Flats" element={<Flats />} />
         <Route path="/CreateGroup" element={<CreateGroup />} />
+        <Route path="/FlatView" element={<FlatView />} />
       </Routes>
     </>
   );
---
+++ client/src/FlatView.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FlatView = () => {
+  return <div>FlatView</div>;
+};
+
+export default FlatView;
