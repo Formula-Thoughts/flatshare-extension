41b598806f13032f6433acc1fce1379714d1e7c4
aidangannon <aidanwilliamgannon@gmail.com>
2024-02-27 10:58:47 +0000

    tweak structure

--- backend/src/abstractions.py
+++ backend/src/abstractions.py
@@ -3,19 +3,7 @@ import uuid
 from dataclasses import dataclass, field
 from typing import Protocol
 
-from backend.src.domain.models import Group
-
 T = typing.TypeVar("T")
-
-
-class BlobRepo(Protocol[T]):
-    def create(self, data: T, key_gen: typing.Callable[[T], str]) -> None:
-        ...
-
-
-class GroupRepo(Protocol[T]):
-    def create(self, data: Group) -> None:
-        ...
 
 
 def uuid4_str():
@@ -31,8 +19,18 @@ class Flat:
 
 @dataclass(unsafe_hash=True)
 class Group:
-    code: int = None
+    id: str = field(default_factory=uuid4_str)
     flats: list[Flat] = field(default_factory=lambda: [])
 
     def can_add_flat(self, flat: Flat) -> bool:
+        ...
+
+
+class BlobRepo(Protocol[T]):
+    def create(self, data: T, key_gen: typing.Callable[[T], str]) -> None:
+        ...
+
+
+class GroupRepo(Protocol[T]):
+    def create(self, data: Group) -> None:
         ...
