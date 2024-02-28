cf4b6cb0f96cb0d9b90da65582249e12c95ed7f1
aidangannon <aidanwilliamgannon@gmail.com>
2024-02-28 01:21:09 +0000

    test event handlers

--- backend/src/events/handlers.py
+++ backend/src/events/handlers.py
@@ -3,7 +3,7 @@ from formula_thoughts_web.application import TopLevelSequenceRunner
 from formula_thoughts_web.crosscutting import ObjectMapper
 from formula_thoughts_web.events import EventHandlerBase
 
-from src.core import Group, IUpsertGroupBackgroundSequenceBuilder
+from src.core import Group, IUpsertGroupBackgroundSequenceBuilder, UserGroups
 
 
 class UpsertGroupEventHandler(EventHandlerBase):
@@ -13,6 +13,19 @@ class UpsertGroupEventHandler(EventHandlerBase):
                  deserializer: Deserializer,
                  object_mapper: ObjectMapper):
         super().__init__(Group,
+                         sequence,
+                         command_pipeline,
+                         deserializer,
+                         object_mapper)
+
+
+class UpsertUserGroupsEventHandler(EventHandlerBase):
+
+    def __init__(self, sequence: IUpsertGroupBackgroundSequenceBuilder,
+                 command_pipeline: TopLevelSequenceRunner,
+                 deserializer: Deserializer,
+                 object_mapper: ObjectMapper):
+        super().__init__(UserGroups,
                          sequence,
                          command_pipeline,
                          deserializer,
--- backend/tests/test_event_handlers.py
+++ backend/tests/test_event_handlers.py
@@ -1,8 +1,8 @@
 from unittest import TestCase
 from unittest.mock import Mock
 
-from src.core import Group
-from src.events.handlers import UpsertGroupEventHandler
+from src.core import Group, UserGroups
+from src.events.handlers import UpsertGroupEventHandler, UpsertUserGroupsEventHandler
 
 
 class TestUpsertGroupEventHandler(TestCase):
@@ -19,3 +19,19 @@ class TestUpsertGroupEventHandler(TestCase):
 
         # assert
         self.assertEqual(event, Group)
+
+
+class TestUpsertUserGroupsEventHandler(TestCase):
+
+    def setUp(self):
+        self.__sut = UpsertUserGroupsEventHandler(sequence=Mock(),
+                                                  command_pipeline=Mock(),
+                                                  deserializer=Mock(),
+                                                  object_mapper=Mock())
+
+    def test_event_should_be_user_groups(self):
+        # act
+        event = self.__sut.event_type
+
+        # assert
+        self.assertEqual(event, UserGroups)
