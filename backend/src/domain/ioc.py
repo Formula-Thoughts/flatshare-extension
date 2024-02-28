5e169ea652edb9d1c6933d7b8481198818b08484
aidangannon <aidanwilliamgannon@gmail.com>
2024-02-28 01:22:57 +0000

    add ioc

--- backend/src/domain/ioc.py
+++ backend/src/domain/ioc.py
@@ -1,9 +1,10 @@
 from formula_thoughts_web.ioc import Container
 
 from src.core import ISetGroupRequestCommand, IValidateGroupCommand, ICreateGroupAsyncCommand, \
-    IUpsertGroupBackgroundCommand, ICreateGroupSequenceBuilder, IUpsertGroupBackgroundSequenceBuilder
+    IUpsertGroupBackgroundCommand, ICreateGroupSequenceBuilder, IUpsertGroupBackgroundSequenceBuilder, \
+    ICreateUserGroupsAsyncCommand
 from src.domain.commands import SetGroupRequestCommand, ValidateGroupCommand, CreateGroupAsyncCommand, \
-    UpsertGroupBackgroundCommand
+    UpsertGroupBackgroundCommand, CreateUserGroupsAsyncCommand
 from src.domain.sequence_builders import CreateGroupSequenceBuilder, UpsertGroupBackgroundSequenceBuilder
 
 
@@ -11,6 +12,7 @@ def register_domain_dependencies(container: Container):
     (container.register(service=ISetGroupRequestCommand, implementation=SetGroupRequestCommand)
      .register(service=IValidateGroupCommand, implementation=ValidateGroupCommand)
      .register(service=ICreateGroupAsyncCommand, implementation=CreateGroupAsyncCommand)
+     .register(service=ICreateUserGroupsAsyncCommand, implementation=CreateUserGroupsAsyncCommand)
      .register(service=IUpsertGroupBackgroundCommand, implementation=UpsertGroupBackgroundCommand)
      .register(service=ICreateGroupSequenceBuilder, implementation=CreateGroupSequenceBuilder)
      .register(service=IUpsertGroupBackgroundSequenceBuilder, implementation=UpsertGroupBackgroundSequenceBuilder))
