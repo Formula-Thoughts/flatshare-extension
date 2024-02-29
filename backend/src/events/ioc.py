1c4ac66b87bfb80a6906a461c46178c50bab4eda
aidangannon <aidanwilliamgannon@gmail.com>
2024-02-29 00:37:06 +0000

    add missing dependency for sqs publisher

--- backend/src/events/ioc.py
+++ backend/src/events/ioc.py
@@ -2,6 +2,7 @@ import os
 
 import boto3
 from formula_thoughts_web.abstractions import EventHandler, Serializer
+from formula_thoughts_web.crosscutting import ObjectMapper
 from formula_thoughts_web.events import SQSEventPublisher
 from formula_thoughts_web.ioc import Container
 
@@ -12,9 +13,11 @@ sqs = boto3.client('sqs')
 
 def register_event_dependencies(container: Container):
     serializer = container.resolve(service=Serializer)
+    mapper = container.resolve(service=ObjectMapper)
     (container.register(service=EventHandler, implementation=UpsertGroupEventHandler)
      .register(service=EventHandler, implementation=UpsertUserGroupsEventHandler)
      # TODO: figure out how to get implementations from service container
      .register_factory(service=SQSEventPublisher, factory=lambda: SQSEventPublisher(sqs_client=sqs,
                                                                                     queue_name=os.environ['QUEUE_NAME'],
-                                                                                    serializer=serializer)))
+                                                                                    serializer=serializer,
+                                                                                    mapper=mapper)))
