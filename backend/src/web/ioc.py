72f49cfc1b2c310b6a5d0de3a81f85dc3ce7e925
aidangannon <aidanwilliamgannon@gmail.com>
2024-02-29 23:56:22 +0000

    add web handler to ioc

--- backend/src/web/ioc.py
+++ backend/src/web/ioc.py
@@ -3,12 +3,13 @@ from formula_thoughts_web.ioc import Container
 
 from src.domain.errors import InvalidGroupError, UserGroupsNotFoundError
 from src.domain.responses import CreatedGroupResponse, ListUserGroupsResponse
-from src.web.handlers import CreateGroupApiHandler, FetchUserGroupsApiHandler
+from src.web.handlers import CreateGroupApiHandler, FetchUserGroupsApiHandler, CreateFlatApiHandler
 
 
 def register_web_dependencies(container: Container):
     (container.register(service=ApiRequestHandler, implementation=CreateGroupApiHandler)
      .register(service=ApiRequestHandler, implementation=FetchUserGroupsApiHandler)
+     .register(service=ApiRequestHandler, implementation=CreateFlatApiHandler)
      .register_status_code_mappings(mappings={
          InvalidGroupError: 400,
          CreatedGroupResponse: 201,
