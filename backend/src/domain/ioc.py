f083cd5c276d12e87eaac1a6ff937deae19c6cc0
aidangannon <aidanwilliamgannon@gmail.com>
2024-02-29 20:36:29 +0000

    add command to ioc

--- backend/src/domain/ioc.py
+++ backend/src/domain/ioc.py
@@ -3,10 +3,11 @@ from formula_thoughts_web.ioc import Container
 from src.core import ISetGroupRequestCommand, IValidateGroupCommand, ICreateGroupAsyncCommand, \
     IUpsertGroupBackgroundCommand, ICreateGroupSequenceBuilder, IUpsertGroupBackgroundSequenceBuilder, \
     ICreateUserGroupsAsyncCommand, IUpsertUserGroupsBackgroundSequenceBuilder, IUpsertUserGroupsBackgroundCommand, \
-    IFetchUserGroupsCommand, IFetchUserGroupsSequenceBuilder, IValidateIfUserBelongsToAtLeastOneGroupCommand
+    IFetchUserGroupsCommand, IFetchUserGroupsSequenceBuilder, IValidateIfUserBelongsToAtLeastOneGroupCommand, \
+    IValidateIfGroupBelongsToUser
 from src.domain.commands import SetGroupRequestCommand, ValidateGroupCommand, CreateGroupAsyncCommand, \
     UpsertGroupBackgroundCommand, CreateUserGroupsAsyncCommand, UpsertUserGroupsBackgroundCommand, \
-    FetchUserGroupsCommand, ValidateIfUserBelongsToAtLeastOneGroupCommand
+    FetchUserGroupsCommand, ValidateIfUserBelongsToAtLeastOneGroupCommand, ValidateIfGroupBelongsToUser
 from src.domain.sequence_builders import CreateGroupSequenceBuilder, UpsertGroupBackgroundSequenceBuilder, \
     UpsertUserGroupsBackgroundSequenceBuilder, FetchUserGroupsSequenceBuilder
 
@@ -22,5 +23,9 @@ def register_domain_dependencies(container: Container):
      .register(service=ICreateGroupSequenceBuilder, implementation=CreateGroupSequenceBuilder)
      .register(service=IUpsertGroupBackgroundSequenceBuilder, implementation=UpsertGroupBackgroundSequenceBuilder)
      .register(service=IFetchUserGroupsSequenceBuilder, implementation=FetchUserGroupsSequenceBuilder)
-     .register(service=IValidateIfUserBelongsToAtLeastOneGroupCommand, implementation=ValidateIfUserBelongsToAtLeastOneGroupCommand)
-     .register(service=IUpsertUserGroupsBackgroundSequenceBuilder, implementation=UpsertUserGroupsBackgroundSequenceBuilder))
+     .register(service=IValidateIfGroupBelongsToUser,
+               implementation=ValidateIfGroupBelongsToUser)
+     .register(service=IValidateIfUserBelongsToAtLeastOneGroupCommand,
+               implementation=ValidateIfUserBelongsToAtLeastOneGroupCommand)
+     .register(service=IUpsertUserGroupsBackgroundSequenceBuilder,
+               implementation=UpsertUserGroupsBackgroundSequenceBuilder))
