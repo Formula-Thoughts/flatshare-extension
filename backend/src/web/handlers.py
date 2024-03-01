8cad62fc85f879392f5dd2b021031cbc70c387db
aidangannon <aidanwilliamgannon@gmail.com>
2024-03-01 00:03:35 +0000

    use create flat sequence builder

--- backend/src/web/handlers.py
+++ backend/src/web/handlers.py
@@ -2,7 +2,7 @@ from formula_thoughts_web.abstractions import Deserializer
 from formula_thoughts_web.application import TopLevelSequenceRunner
 from formula_thoughts_web.web import ApiRequestHandlerBase
 
-from src.core import ICreateGroupSequenceBuilder, IFetchUserGroupsSequenceBuilder
+from src.core import ICreateGroupSequenceBuilder, IFetchUserGroupsSequenceBuilder, ICreateFlatSequenceBuilder
 
 
 class CreateGroupApiHandler(ApiRequestHandlerBase):
@@ -18,7 +18,7 @@ class CreateGroupApiHandler(ApiRequestHandlerBase):
 
 class CreateFlatApiHandler(ApiRequestHandlerBase):
 
-    def __init__(self, sequence: ICreateGroupSequenceBuilder,
+    def __init__(self, sequence: ICreateFlatSequenceBuilder,
                  command_pipeline: TopLevelSequenceRunner,
                  deserializer: Deserializer):
         super().__init__(route_key='POST /groups/{group_id}/flats',
