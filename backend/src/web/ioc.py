ad208f82a1cc6e96cb7b3efcc0b7a3d252271eb9
aidangannon <aidanwilliamgannon@gmail.com>
2024-03-02 01:56:01 +0000

    register handlers and responses

--- backend/src/web/ioc.py
+++ backend/src/web/ioc.py
@@ -2,9 +2,9 @@ from formula_thoughts_web.abstractions import ApiRequestHandler
 from formula_thoughts_web.ioc import Container
 
 from src.domain.errors import InvalidGroupDataError, UserGroupsNotFoundError, GroupNotFoundError, FlatNotFoundError
-from src.domain.responses import CreatedGroupResponse, ListUserGroupsResponse, SingleGroupResponse
+from src.domain.responses import CreatedGroupResponse, ListUserGroupsResponse, SingleGroupResponse, GetGroupCodeResponse
 from src.web.handlers import CreateGroupApiHandler, FetchUserGroupsApiHandler, CreateFlatApiHandler, \
-    DeleteFlatApiHandler, AddCurrentUserToGroupApiHandler
+    DeleteFlatApiHandler, AddCurrentUserToGroupApiHandler, GetCodeForGroupApiHandler
 
 
 def register_web_dependencies(container: Container):
@@ -13,10 +13,12 @@ def register_web_dependencies(container: Container):
      .register(service=ApiRequestHandler, implementation=CreateFlatApiHandler)
      .register(service=ApiRequestHandler, implementation=DeleteFlatApiHandler)
      .register(service=ApiRequestHandler, implementation=AddCurrentUserToGroupApiHandler)
+     .register(service=ApiRequestHandler, implementation=GetCodeForGroupApiHandler)
      .register_status_code_mappings(mappings={
          InvalidGroupDataError: 400,
          CreatedGroupResponse: 201,
          ListUserGroupsResponse: 200,
+         GetGroupCodeResponse: 200,
          SingleGroupResponse: 200,
          UserGroupsNotFoundError: 404,
          GroupNotFoundError: 404,
