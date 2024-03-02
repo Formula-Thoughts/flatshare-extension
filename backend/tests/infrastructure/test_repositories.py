48dfc7df0768f973f5f7e28d44dffbc59f9ceda1
aidangannon <aidanwilliamgannon@gmail.com>
2024-03-02 17:55:19 +0000

    amend repo tests

--- backend/tests/infrastructure/test_repositories.py
+++ backend/tests/infrastructure/test_repositories.py
@@ -5,6 +5,7 @@ from unittest.mock import patch
 
 import boto3
 from autofixture import AutoFixture
+from botocore.client import BaseClient
 from formula_thoughts_web.crosscutting import JsonSnakeToCamelSerializer, JsonCamelToSnakeDeserializer, ObjectMapper
 from moto import mock_aws
 
@@ -18,15 +19,13 @@ BUCKET = "test-bucket"
 
 class S3TestCase(TestCase):
 
-    def setUp(self) -> None:
+    def _set_up_bucket(self):
         self.__s3 = boto3.client('s3')
         self.__s3_client_wrapper = S3ClientWrapper(client=self.__s3)
         self._blob_repo = S3BlobRepo(s3_client_wrapper=self.__s3_client_wrapper,
                                      serializer=JsonSnakeToCamelSerializer(),
                                      deserializer=JsonCamelToSnakeDeserializer(),
                                      object_mapper=ObjectMapper())
-
-    def _set_up_bucket(self):
         self.__s3.create_bucket(Bucket=BUCKET,
                                 CreateBucketConfiguration={
                                     'LocationConstraint': 'eu-west-2',
@@ -35,21 +34,18 @@ class S3TestCase(TestCase):
 
 class TestGroupRepo(S3TestCase):
 
-    def setUp(self):
-        super().setUp()
-        self.__sut = S3GroupRepo(blob_repo=self._blob_repo)
-
 
     @mock_aws
     @patch.dict(os.environ, {"S3_BUCKET_NAME": BUCKET}, clear=True)
     def test_get_group_by_id(self):
         # arrange
         self._set_up_bucket()
+        sut = S3GroupRepo(blob_repo=self._blob_repo)
         stored_group = AutoFixture().create(dto=Group)
-        self.__sut.create(group=stored_group)
+        sut.create(group=stored_group)
 
         # act
-        fetched_group = self.__sut.get(_id=stored_group.id)
+        fetched_group = sut.get(_id=stored_group.id)
 
         # assert
         self.assertEqual(fetched_group, stored_group)
@@ -59,17 +55,14 @@ class TestGroupRepo(S3TestCase):
     def test_get_group_by_id_when_not_found(self):
         # arrange
         self._set_up_bucket()
+        sut = S3GroupRepo(blob_repo=self._blob_repo)
 
         # act/assert
         with self.assertRaises(expected_exception=GroupNotFoundException):
-            fetched_group = self.__sut.get(_id=str(uuid.uuid4()))
+            sut.get(_id=str(uuid.uuid4()))
 
 
 class TestUserGroupsRepo(S3TestCase):
-
-    def setUp(self):
-        super().setUp()
-        self.__sut = S3UserGroupsRepo(blob_repo=self._blob_repo)
 
 
     @mock_aws
@@ -77,11 +70,12 @@ class TestUserGroupsRepo(S3TestCase):
     def test_get_user_groups_by_auth_id(self):
         # arrange
         self._set_up_bucket()
+        sut = S3UserGroupsRepo(blob_repo=self._blob_repo)
         stored_user_groups = AutoFixture().create(dto=UserGroups)
-        self.__sut.create(user_groups=stored_user_groups)
+        sut.create(user_groups=stored_user_groups)
 
         # act
-        fetched_user_groups = self.__sut.get(_id=stored_user_groups.auth_user_id)
+        fetched_user_groups = sut.get(_id=stored_user_groups.auth_user_id)
 
         # assert
         self.assertEqual(fetched_user_groups, stored_user_groups)
@@ -91,7 +85,8 @@ class TestUserGroupsRepo(S3TestCase):
     def test_get_user_groups_by_auth_id_when_not_found(self):
         # arrange
         self._set_up_bucket()
+        sut = S3UserGroupsRepo(blob_repo=self._blob_repo)
 
         # act/assert
         with self.assertRaises(expected_exception=UserGroupsNotFoundException):
-            fetched_group = self.__sut.get(_id=str(uuid.uuid4()))
+            sut.get(_id=str(uuid.uuid4()))
