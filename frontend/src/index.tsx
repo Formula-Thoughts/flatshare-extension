dc181a05e642d7c8ca07bfb91ceb326df9471983
Dommo <dominicfarr@gmail.com>
2024-03-03 16:41:14 -0400

    Added a simple PoC for invite routes

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,10 +4,37 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import { Amplify } from "aws-amplify";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      loginWith: {
+        oauth: {
+          scopes: ["email", "openid", "profile"],
+          redirectSignIn: [
+            "https://localhost:3000",
+            "https://flatini.formulathoughts.com",
+          ],
+          redirectSignOut: [
+            "https://localhost:3000",
+            "https://localhost:3000/",
+            "https://flatini.formulathoughts.com",
+          ],
+          responseType: "code",
+          domain: "flatini.auth.eu-west-2.amazoncognito.com/",
+        },
+      },
+      userPoolId: "eu-west-2_U2HUbgIZK",
+      userPoolClientId: "95rl77bq6dosrgerjfkgm12f5",
+    },
+  },
+});
+
 root.render(
   <React.StrictMode>
     <Authenticator.Provider>
