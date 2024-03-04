a3afa5d3859975622071530f08be100aa177c4ad
aidangannon <aidanwilliamgannon@gmail.com>
2024-03-04 22:25:18 +0000

    add to ioc

--- backend/src/web/ioc.py
+++ backend/src/web/ioc.py
@@ -4,11 +4,13 @@ from formula_thoughts_web.ioc import Container
 from src.domain.errors import InvalidGroupDataError, UserGroupsNotFoundError, GroupNotFoundError, FlatNotFoundError
 from src.domain.responses import CreatedGroupResponse, ListUserGroupsResponse, SingleGroupResponse, GetGroupCodeResponse
 from src.web.handlers import UpdateGroupApiHandler, FetchUserGroupsApiHandler, CreateFlatApiHandler, \
-    DeleteFlatApiHandler, AddCurrentUserToGroupApiHandler, GetCodeForGroupApiHandler, GetUserGroupByIdApiHandler
+    DeleteFlatApiHandler, AddCurrentUserToGroupApiHandler, GetCodeForGroupApiHandler, GetUserGroupByIdApiHandler, \
+    CreateGroupApiHandler
 
 
 def register_web_dependencies(container: Container):
     (container.register(service=ApiRequestHandler, implementation=UpdateGroupApiHandler)
+     .register(service=ApiRequestHandler, implementation=CreateGroupApiHandler)
      .register(service=ApiRequestHandler, implementation=FetchUserGroupsApiHandler)
      .register(service=ApiRequestHandler, implementation=CreateFlatApiHandler)
      .register(service=ApiRequestHandler, implementation=DeleteFlatApiHandler)
