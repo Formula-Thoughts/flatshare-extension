7cbb579bf7610458ab3dbc0859be565cc86e0d8b
Xavier Mod <hi.xavier.mod@gmail.com>
2024-04-27 08:44:12 +0100

    Fix URL

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,3 +1,2 @@
-export const flatiniAuthWebsite =
-  process.env.REACT_APP_FLATINI_WEB_DEV ||
-  "https://flatini.formulathoughts.com/";
+export const flatiniAuthWebsite = "https://flatini.formulathoughts.com/";
+// add process.env.REACT_APP_FLATINI_WEB_DEV for dev
