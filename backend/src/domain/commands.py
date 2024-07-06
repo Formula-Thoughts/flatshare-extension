5c2e7b3538333f66975df710ba21fdf3995af6c2
aidangannon <aidanwilliamgannon@gmail.com>
2024-07-06 20:52:02 +0100

    implement down vote

--- backend/src/domain/commands.py
+++ backend/src/domain/commands.py
@@ -454,4 +454,4 @@ class DeleteVoteCommand:
     def run(self, context: ApplicationContext) -> None:
         red_flag = context.get_var(name=RED_FLAG, _type=RedFlag)
         user = context.auth_user_id
-        self.__red_flag_repo.add_voter(user_id=user, red_flag=red_flag)
+        self.__red_flag_repo.remove_voter(user_id=user, red_flag=red_flag)
