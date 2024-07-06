66744e6be476c5b8591ddfb1009deba42d1a79d2
aidangannon <aidanwilliamgannon@gmail.com>
2024-07-06 20:09:35 +0100

    implement create vote sequence

--- backend/src/domain/sequence_builders.py
+++ backend/src/domain/sequence_builders.py
@@ -219,6 +219,7 @@ class CreateVoteForRedFlagSequenceBuilder(FluentSequenceBuilder):
         self._add_command(command=self.__validate_get_red_flags_request) \
             ._add_command(command=self.__get_red_flag_by_id) \
             ._add_command(command=self.__validate_not_voted) \
+            ._add_command(command=self.__upvote) \
             ._add_command(command=self.__set_anonymous_red_flag_response)
 
 
