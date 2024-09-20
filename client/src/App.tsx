0b5d2f8f7ddc688a8f02ace458da5ff3f0f239c1
Xavier Mod <hi.xavier.mod@gmail.com>
2024-09-20 12:52:04 +0100

    Progress

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -20,7 +20,7 @@ function App() {
   let url;
   const setActiveUrl = () => {
     console.log(
-      "3 [setActiveUrl - App.tsx] -> Sets active ULR",
+      "3 [setActiveUrl - App.tsx] -> Sets active URL",
       state.isGroupLoading
     );
 
--- client/src/FlatView.tsx
+++ client/src/FlatView.tsx
@@ -171,7 +171,7 @@ const FlatView = () => {
   const getRedFlags = async () => {
     const redFlags = await _getPropertyRedFlags(
       userAuthToken as string,
-      activeFlatData.url
+      activeFlatData?.url
     );
     setActiveFlatData({ ...activeFlatData, redFlags: redFlags.redFlags });
     console.log(
@@ -181,10 +181,15 @@ const FlatView = () => {
   };
 
   useEffect(() => {
-    console.log("4 [useEffect - FlatView.tsx] -> Flatview loads");
-    getDataFromActiveTab();
+    if (activeUrl?.contents) {
+      setLoadingFlatData(true);
+      console.log(
+        "4 [useEffect - FlatView.tsx] -> Flatview loads, and watches for changes on activeUrl.contents"
+      );
+      getDataFromActiveTab();
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [activeUrl?.contents]);
 
   useEffect(() => {
     if (activeFlatData?.url) {
@@ -210,77 +215,52 @@ const FlatView = () => {
     return <Loading />;
   }
 
-  if (!activeFlatData.price || !activeFlatData.title || !activeFlatData.url) {
+  if (
+    !activeFlatData?.price ||
+    !activeFlatData?.title ||
+    !activeFlatData?.url
+  ) {
     setAppHasError(
       "Sorry. This property cannot be added to your list, would you mind reloading the extension? :)"
     );
   }
-  return (
-    <Wrapper
-      doesFlatMeetRequirements={
-        checkIfPropertyMeetsRequirements(
-          extractNumber(activeFlatData.price),
-          activeFlatData.title
-        ).location &&
-        checkIfPropertyMeetsRequirements(
-          extractNumber(activeFlatData.price),
-          activeFlatData.title
-        ).price
-      }
-      isFlatDuplicated={isFlatDuplicated}
-    >
-      <Link to="/">
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            gap: "0.5rem",
-            cursor: "pointer",
-            opacity: 0.7,
-          }}
-        >
-          <FaArrowLeft />
-          <Text type={TextTypes.paragraph}>Back to my list</Text>
-        </div>
-      </Link>
-      <Text type={TextTypes.title}>{activeFlatData.title}</Text>
-      <InfoWrapper>
-        {/* Location */}
-        {checkIfPropertyMeetsRequirements(
-          extractNumber(activeFlatData.price),
-          activeFlatData.title
-        ).location ? (
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <Image
-              style={{ width: "2.3rem", marginRight: "1rem" }}
-              src={checkmark}
-              alt="checkmark"
-            />
-            <Text type={TextTypes.paragraph}>
-              Meets your{" "}
-              <span style={{ fontWeight: "bold" }}>desired location</span>.
-            </Text>
-          </div>
-        ) : (
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <Image
-              style={{ width: "2.3rem", marginRight: "1rem" }}
-              src={cross}
-              alt="cross"
-            />
-            <Text type={TextTypes.paragraph}>
-              Does not meet your{" "}
-              <span style={{ fontWeight: "bold" }}>desired location</span>.
-            </Text>
-          </div>
-        )}
-        {/* Price */}
-        {requirements.price ? (
+  if (activeFlatData) {
+    return (
+      <Wrapper
+        doesFlatMeetRequirements={
           checkIfPropertyMeetsRequirements(
-            extractNumber(activeFlatData.price),
-            activeFlatData.title
-          ).price ? (
+            extractNumber(activeFlatData?.price),
+            activeFlatData?.title
+          ).location &&
+          checkIfPropertyMeetsRequirements(
+            extractNumber(activeFlatData?.price),
+            activeFlatData?.title
+          ).price
+        }
+        isFlatDuplicated={isFlatDuplicated}
+      >
+        <Link to="/">
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: "0.5rem",
+              cursor: "pointer",
+              opacity: 0.7,
+            }}
+          >
+            <FaArrowLeft />
+            <Text type={TextTypes.paragraph}>Back to my list</Text>
+          </div>
+        </Link>
+        <Text type={TextTypes.title}>{activeFlatData?.title}</Text>
+        <InfoWrapper>
+          {/* Location */}
+          {checkIfPropertyMeetsRequirements(
+            extractNumber(activeFlatData?.price),
+            activeFlatData?.title
+          ).location ? (
             <div style={{ display: "flex", alignItems: "center" }}>
               <Image
                 style={{ width: "2.3rem", marginRight: "1rem" }}
@@ -288,11 +268,8 @@ const FlatView = () => {
                 alt="checkmark"
               />
               <Text type={TextTypes.paragraph}>
-                Under{" "}
-                <span style={{ fontWeight: "bold" }}>
-                  £{requirements.price}
-                </span>{" "}
-                per month.
+                Meets your{" "}
+                <span style={{ fontWeight: "bold" }}>desired location</span>.
               </Text>
             </div>
           ) : (
@@ -303,99 +280,135 @@ const FlatView = () => {
                 alt="cross"
               />
               <Text type={TextTypes.paragraph}>
-                Not under{" "}
-                <span style={{ fontWeight: "bold" }}>
-                  £{requirements.price}
-                </span>{" "}
-                per month.
+                Does not meet your{" "}
+                <span style={{ fontWeight: "bold" }}>desired location</span>.
               </Text>
             </div>
-          )
-        ) : null}
-      </InfoWrapper>
-      <div>
-        {isFlatDuplicated ? (
-          <Button onClick={removeFlatFromList} label="Remove from list" />
-        ) : Object.values(
+          )}
+          {/* Price */}
+          {requirements.price ? (
             checkIfPropertyMeetsRequirements(
-              extractNumber(activeFlatData.price),
-              activeFlatData.title
+              extractNumber(activeFlatData?.price),
+              activeFlatData?.title
+            ).price ? (
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <Image
+                  style={{ width: "2.3rem", marginRight: "1rem" }}
+                  src={checkmark}
+                  alt="checkmark"
+                />
+                <Text type={TextTypes.paragraph}>
+                  Under{" "}
+                  <span style={{ fontWeight: "bold" }}>
+                    £{requirements.price}
+                  </span>{" "}
+                  per month.
+                </Text>
+              </div>
+            ) : (
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <Image
+                  style={{ width: "2.3rem", marginRight: "1rem" }}
+                  src={cross}
+                  alt="cross"
+                />
+                <Text type={TextTypes.paragraph}>
+                  Not under{" "}
+                  <span style={{ fontWeight: "bold" }}>
+                    £{requirements.price}
+                  </span>{" "}
+                  per month.
+                </Text>
+              </div>
             )
-          ).some((value) => value === false) ? (
+          ) : null}
+        </InfoWrapper>
+        <div>
+          {isFlatDuplicated ? (
+            <Button onClick={removeFlatFromList} label="Remove from list" />
+          ) : Object.values(
+              checkIfPropertyMeetsRequirements(
+                extractNumber(activeFlatData?.price),
+                activeFlatData?.title
+              )
+            ).some((value) => value === false) ? (
+            <Button
+              onClick={async () => {
+                await addFlat(
+                  activeFlatData?.url,
+                  activeFlatData?.price,
+                  activeFlatData?.title
+                );
+                setIsFlatDuplicated(true);
+              }}
+              label="Add to the list anyway"
+            />
+          ) : (
+            <Button
+              onClick={async () => {
+                await addFlat(
+                  activeFlatData?.url,
+                  activeFlatData?.price,
+                  activeFlatData?.title
+                );
+                setIsFlatDuplicated(true);
+              }}
+              label="Add to the list"
+            />
+          )}
+        </div>
+        <div>
+          <p>
+            Price: <span style={{ opacity: 0.7 }}>{activeFlatData?.price}</span>
+          </p>
+          <p style={{ marginTop: 5 }}>
+            Location:{" "}
+            <span style={{ opacity: 0.7 }}>{activeFlatData?.title}</span>
+          </p>
+        </div>
+        {activeFlatData?.redFlags?.length === 0 ? (
           <Button
-            onClick={async () => {
-              await addFlat(
-                activeFlatData.url,
-                activeFlatData.price,
-                activeFlatData.title
-              );
-              setIsFlatDuplicated(true);
+            style={{
+              width: "100%",
             }}
-            label="Add to the list anyway"
+            onClick={() =>
+              navigate("/AddRedFlag", {
+                state: {
+                  flatName: activeFlatData?.title,
+                  flatUrl: activeFlatData?.url,
+                },
+              })
+            }
+            label="🚩 Add red flag"
           />
         ) : (
-          <Button
-            onClick={async () => {
-              await addFlat(
-                activeFlatData.url,
-                activeFlatData.price,
-                activeFlatData.title
-              );
-              setIsFlatDuplicated(true);
-            }}
-            label="Add to the list"
-          />
+          <RedFlagsBanner onClick={() => navigate("/RedFlags")}>
+            <span style={{ fontSize: "2rem" }}>🚩</span>
+            <div>
+              <Text type={TextTypes.paragraph}>
+                Some users have spotted {activeFlatData?.redFlags?.length} red
+                flag(s) in this property.
+              </Text>
+              <Text
+                style={{
+                  position: "absolute",
+                  right: 0,
+                  bottom: 0,
+                  opacity: 0.5,
+                  margin: "0.5rem",
+                  fontSize: "1rem",
+                }}
+                type={TextTypes.paragraph}
+              >
+                See ({activeFlatData?.redFlags?.length})
+              </Text>
+            </div>
+          </RedFlagsBanner>
         )}
-      </div>
-      <div>
-        <p>
-          Price: <span style={{ opacity: 0.7 }}>{activeFlatData.price}</span>
-        </p>
-        <p style={{ marginTop: 5 }}>
-          Location: <span style={{ opacity: 0.7 }}>{activeFlatData.title}</span>
-        </p>
-      </div>
-      {activeFlatData?.redFlags?.length === 0 ? (
-        <Button
-          style={{
-            width: "100%",
-          }}
-          onClick={() =>
-            navigate("/AddRedFlag", {
-              state: {
-                flatName: activeFlatData.title,
-                flatUrl: activeFlatData.url,
-              },
-            })
-          }
-          label="🚩 Add red flag"
-        />
-      ) : (
-        <RedFlagsBanner onClick={() => navigate("/RedFlags")}>
-          <span style={{ fontSize: "2rem" }}>🚩</span>
-          <div>
-            <Text type={TextTypes.paragraph}>
-              Some users have spotted {activeFlatData?.redFlags?.length} red
-              flag(s) in this property.
-            </Text>
-            <Text
-              style={{
-                position: "absolute",
-                right: 0,
-                bottom: 0,
-                opacity: 0.5,
-                margin: "0.5rem",
-                fontSize: "1rem",
-              }}
-              type={TextTypes.paragraph}
-            >
-              See ({activeFlatData?.redFlags?.length})
-            </Text>
-          </div>
-        </RedFlagsBanner>
-      )}
-    </Wrapper>
-  );
+      </Wrapper>
+    );
+  }
+  return <div />;
 };
 
 export default FlatView;
