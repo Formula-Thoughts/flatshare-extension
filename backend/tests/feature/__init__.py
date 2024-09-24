f6cb4ec4855797f4d20ad32c740c9026b26b9ee4
aidangannon <aidanwilliamgannon@gmail.com>
2024-09-24 20:49:53 +0100

    comment tests

--- backend/tests/feature/__init__.py
+++ backend/tests/feature/__init__.py
@@ -27,86 +27,86 @@ class Response:
     status: HTTPStatus = None
 
 
-@patch.dict(os.environ, MOCKED_OS_ENVIRON, clear=True)
-class Feature:
-
-    def setUp(self):
-        self.__table_name = 'flatini-test'
-        self.__default_region = "eu-west-2"
-        self._container = Container()
-        bootstrap(container=self._container)
-        self.__dynamo = boto3.resource('dynamodb', region_name=self.__default_region)
-        self._cognito = boto3.client('cognito-idp', region_name=self.__default_region)
-        pool = self._cognito.create_user_pool(
-            PoolName=USER_POOL,
-            Schema=[
-                {
-                    'Name': 'name',
-                    'AttributeDataType': 'String',
-                    'Required': True,
-                    'Mutable': True
-                },
-                {
-                    'Name': 'email',
-                    'AttributeDataType': 'String',
-                    'Required': True,
-                    'Mutable': True
-                }
-            ],
-            AutoVerifiedAttributes=['email'],
-            UsernameConfiguration={
-                'CaseSensitive': False
-            }
-        )
-        cognito_pool_id = pool["UserPool"]["Id"]
-        self._user_pool_id = cognito_pool_id
-        os.environ["USER_POOL_ID"] = self._user_pool_id
-        self._dynamo_client_wrapper = DynamoDbWrapper(dynamo_client=self.__dynamo,
-                                                      tablename=self.__table_name)
-        (self._container
-         .register_factory(service=DynamoDbWrapper,
-                           factory=lambda: self._dynamo_client_wrapper)
-         .register_factory(service=CognitoClientWrapper,
-                           factory=lambda: CognitoClientWrapper(client=self._cognito)))
-        self.__dynamo.create_table(TableName=self.__table_name,
-                                   KeySchema=[
-                                       {
-                                           'AttributeName': 'partition_key',
-                                           'KeyType': 'HASH',
-                                       },
-                                       {
-                                           'AttributeName': 'id',
-                                           'KeyType': 'RANGE',
-                                       }
-                                   ],
-                                   AttributeDefinitions=[
-                                       {
-                                           'AttributeName': 'partition_key',
-                                           'AttributeType': 'S'
-                                       },
-                                       {
-                                           'AttributeName': 'id',
-                                           'AttributeType': 'S'
-                                       }
-                                   ],
-                                   BillingMode='PAY_PER_REQUEST')
-
-    def _send_request(self,
-                      route_key: str,
-                      auth_user_id: str,
-                      path_params: dict = None,
-                      params: dict = None,
-                      body: dict = None) -> Response:
-        event = {'routeKey': route_key}
-        if path_params is not None:
-            event['pathParameters'] = path_params
-        if params is not None:
-            event['queryStringParameters'] = params
-        if body is not None:
-            event['body'] = self._container.resolve(service=Serializer).serialize(data=body)
-        response = Response()
-        result = run(event=event, context={}, container=self._container)
-        if result['body'] is not None:
-            response.body = self._container.resolve(service=Deserializer).deserialize(data=result['body'])
-        response.status = HTTPStatus(result['statusCode'])
-        return response
+# @patch.dict(os.environ, MOCKED_OS_ENVIRON, clear=True)
+# class Feature:
+#
+#     def setUp(self):
+#         self.__table_name = 'flatini-test'
+#         self.__default_region = "eu-west-2"
+#         self._container = Container()
+#         bootstrap(container=self._container)
+#         self.__dynamo = boto3.resource('dynamodb', region_name=self.__default_region)
+#         self._cognito = boto3.client('cognito-idp', region_name=self.__default_region)
+#         pool = self._cognito.create_user_pool(
+#             PoolName=USER_POOL,
+#             Schema=[
+#                 {
+#                     'Name': 'name',
+#                     'AttributeDataType': 'String',
+#                     'Required': True,
+#                     'Mutable': True
+#                 },
+#                 {
+#                     'Name': 'email',
+#                     'AttributeDataType': 'String',
+#                     'Required': True,
+#                     'Mutable': True
+#                 }
+#             ],
+#             AutoVerifiedAttributes=['email'],
+#             UsernameConfiguration={
+#                 'CaseSensitive': False
+#             }
+#         )
+#         cognito_pool_id = pool["UserPool"]["Id"]
+#         self._user_pool_id = cognito_pool_id
+#         os.environ["USER_POOL_ID"] = self._user_pool_id
+#         self._dynamo_client_wrapper = DynamoDbWrapper(dynamo_client=self.__dynamo,
+#                                                       tablename=self.__table_name)
+#         (self._container
+#          .register_factory(service=DynamoDbWrapper,
+#                            factory=lambda: self._dynamo_client_wrapper)
+#          .register_factory(service=CognitoClientWrapper,
+#                            factory=lambda: CognitoClientWrapper(client=self._cognito)))
+#         self.__dynamo.create_table(TableName=self.__table_name,
+#                                    KeySchema=[
+#                                        {
+#                                            'AttributeName': 'partition_key',
+#                                            'KeyType': 'HASH',
+#                                        },
+#                                        {
+#                                            'AttributeName': 'id',
+#                                            'KeyType': 'RANGE',
+#                                        }
+#                                    ],
+#                                    AttributeDefinitions=[
+#                                        {
+#                                            'AttributeName': 'partition_key',
+#                                            'AttributeType': 'S'
+#                                        },
+#                                        {
+#                                            'AttributeName': 'id',
+#                                            'AttributeType': 'S'
+#                                        }
+#                                    ],
+#                                    BillingMode='PAY_PER_REQUEST')
+#
+#     def _send_request(self,
+#                       route_key: str,
+#                       auth_user_id: str,
+#                       path_params: dict = None,
+#                       params: dict = None,
+#                       body: dict = None) -> Response:
+#         event = {'routeKey': route_key}
+#         if path_params is not None:
+#             event['pathParameters'] = path_params
+#         if params is not None:
+#             event['queryStringParameters'] = params
+#         if body is not None:
+#             event['body'] = self._container.resolve(service=Serializer).serialize(data=body)
+#         response = Response()
+#         result = run(event=event, context={}, container=self._container)
+#         if result['body'] is not None:
+#             response.body = self._container.resolve(service=Deserializer).deserialize(data=result['body'])
+#         response.status = HTTPStatus(result['statusCode'])
+#         return response
--- backend/tests/feature/test_create_group.py
+++ backend/tests/feature/test_create_group.py
@@ -6,41 +6,41 @@ from formula_thoughts_web.crosscutting import ObjectMapper
 from moto import mock_aws
 
 from src.core import Group, IGroupRepo, IUserGroupsRepo
-from tests.feature import FeatureTestCase, MOCKED_OS_ENVIRON, USER_POOL
+from tests.feature import MOCKED_OS_ENVIRON, USER_POOL
 
 
-class SkipTestCreateGroupSequenceBuilder(FeatureTestCase):
-
-    def setUp(self) -> None:
-        super().setUp()
-        self.__object_mapper = self._container.resolve(service=ObjectMapper)
-        self.__group_repo = self._container.resolve(service=IGroupRepo)
-        self.__user_group_repo = self._container.resolve(service=IUserGroupsRepo)
-
-    def skip_create_group_should_create_group_when_no_groups_exist(self):
-        # arrange
-        route = "POST /groups"
-        auth_user = "test_user"
-        self._cognito.admin_create_user(
-            UserPoolId=self._user_pool_id,
-            Username=auth_user,
-            UserAttributes=[
-                {
-                    "Name": "name",
-                    "Value": "John Doe"
-                },
-            ]
-        )
-
-        # act
-        response = self._send_request(route_key=route, auth_user_id=auth_user)
-
-        # assert
-        with self.subTest(msg="response status is created"):
-            self.assertEqual(response.status, HTTPStatus.CREATED)
-
-        # assert
-        with self.subTest(msg="group in db matches response"):
-            user_groups = self.__user_group_repo.get(_id=auth_user)
-            group = self.__group_repo.get(_id=user_groups.groups[0])
-            self.assertEqual(self.__object_mapper.map_to_dict(_from=group, to=Group), response.content)
+# class SkipTestCreateGroupSequenceBuilder:
+#
+#     def setUp(self) -> None:
+#         super().setUp()
+#         self.__object_mapper = self._container.resolve(service=ObjectMapper)
+#         self.__group_repo = self._container.resolve(service=IGroupRepo)
+#         self.__user_group_repo = self._container.resolve(service=IUserGroupsRepo)
+#
+#     def skip_create_group_should_create_group_when_no_groups_exist(self):
+#         # arrange
+#         route = "POST /groups"
+#         auth_user = "test_user"
+#         self._cognito.admin_create_user(
+#             UserPoolId=self._user_pool_id,
+#             Username=auth_user,
+#             UserAttributes=[
+#                 {
+#                     "Name": "name",
+#                     "Value": "John Doe"
+#                 },
+#             ]
+#         )
+#
+#         # act
+#         response = self._send_request(route_key=route, auth_user_id=auth_user)
+#
+#         # assert
+#         with self.subTest(msg="response status is created"):
+#             self.assertEqual(response.status, HTTPStatus.CREATED)
+#
+#         # assert
+#         with self.subTest(msg="group in db matches response"):
+#             user_groups = self.__user_group_repo.get(_id=auth_user)
+#             group = self.__group_repo.get(_id=user_groups.groups[0])
+#             self.assertEqual(self.__object_mapper.map_to_dict(_from=group, to=Group), response.content)
